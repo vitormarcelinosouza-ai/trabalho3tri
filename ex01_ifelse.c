0ec56c1bb44e6f45e84436ac0c54a06db139cbb2
vitormarcelinosouza-ai <vitor.marcelino.souza@escola.pr.gov.br>
2025-10-27 07:44:42 -0300

    Create ex01_ifelse.c

---
+++ ex01_ifelse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+     int numero;
+     printf ("digite um número\n");
+     scanf ("%d",&numero);
+     if(numero%2==0){
+               
+         printf(" seu numero é par\n");
+            }
+            else{
+               
+                printf(" seu numero é impar\n");
+            }
+}  
